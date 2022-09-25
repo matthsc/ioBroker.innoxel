1f648e0f3515bdf3ab6e3033bcffdff9b38f61ee
Matthias Schwende <matthsc@gmx.net>
2022-09-25 20:46:27 +0200

    remove build files from git

--- build/lib/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asArray = void 0;
-function asArray(obj) {
-    return Array.isArray(obj) ? obj : [obj];
-}
-exports.asArray = asArray;
-//# sourceMappingURL=util.js.map
