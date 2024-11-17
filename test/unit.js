606f3eb7dd208dc6d6b0ff2bd51492a346694692
Matthias Schwende <matthsc@gmx.net>
2024-11-17 16:46:47 +0100

    remove unused and deprecated unit tests

--- test/unit.js
+++
@@ -1,9 +0,0 @@
-import { tests } from "@iobroker/testing";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
-// Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(join(__dirname, ".."));
